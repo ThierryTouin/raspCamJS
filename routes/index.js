7fc9c89ce74d178fe66155e599ff145ef892b802
Thierry Touin <thierrytouin@gmail.com>
2017-12-03 22:04:39 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+let express = require('express')
+let router = express.Router()
+let passport = require('passport')
+
+
+// Define routes.
+router.get('/',
+function(request, response) {
+    response.render('home.ejs', { user: request.user });
+});
+
+router.get('/login',
+function(request, response){
+    response.render('login.ejs');
+});
+
+router.post('/login', 
+passport.authenticate('local', { failureRedirect: 'login.ejs' }),
+function(request, response) {
+    response.redirect('/');
+});
+
+router.get('/logout',
+function(request, response){
+    request.logout();
+  response.redirect('/');
+});
+
+router.get('/profile',
+require('connect-ensure-login').ensureLoggedIn(),
+function(request, response){
+    response.render('profile.ejs', { user: request.user });
+});
+
+router.get('/about',
+require('connect-ensure-login').ensureLoggedIn(),
+function(request, response){
+    response.render('about.ejs', { user: request.user });
+});
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,156 @@
+"use strict";
+
+//require('daemon')();
+
+let express = require('express')
+let app = express()
+let bodyparser = require('body-parser')
+let session = require('express-session')
+
+const errorLog = require('./util/logger').errorlog;
+const successlog = require('./util/logger').successlog;
+
+// Moteur de template
+app.set('view engine', 'ejs')
+
+let passport = require('passport')
+let Strategy = require('passport-local').Strategy
+let db = require('./db')
+
+
+// Configure the local strategy for use by Passport.
+//
+// The local strategy require a `verify` function which receives the credentials
+// (`username` and `password`) submitted by the user.  The function must verify
+// that the password is correct and then invoke `cb` with a user object, which
+// will be set at `req.user` in route handlers after authentication.
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
+
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  The
+// typical implementation of this is as simple as supplying the user ID when
+// serializing, and querying the user record by ID from the database when
+// deserializing.
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+
+
+// Configure view engine to render EJS templates.
+app.set('views', __dirname + '/views/pages');
+app.set('view engine', 'ejs');
+
+// Middleware
+app.use('/assets', express.static('public'))
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+// Routes ======================================================================
+app.use('/', require('./routes'));
+
+// Camera
+var Campi = require('campi');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var base64 = require('base64-stream');
+var numClients = 0;
+var campi = new Campi();
+
+var startCam = 0;
+
+io.on('connection', function(socket) {
+  
+      socket.on("clientMsg", function (data) {
+          successlog.info(`Client user agent : ${data}`);
+      });
+  
+      numClients++;
+      io.emit('stats', { numClients: numClients });
+  
+      successlog.info(`Connected clients: ${numClients}`);
+      //console.log('Connected clients:', numClients);
+  
+      socket.on('disconnect', function() {
+          numClients--;
+          io.emit('stats', { numClients: numClients });
+  
+          successlog.info(`Connected clients: ${numClients}`);
+          //console.log('Connected clients:', numClients);
+      });
+
+      socket.on('startCam', function(socket) {    
+        successlog.info(`startCam()`);
+        startCam = 1;
+      });
+      socket.on('stopCam', function(socket) {    
+        successlog.info(`stopCam()`);
+        startCam = 0;
+      });
+    
+  });
+  
+  
+  http.listen(3000, function () {
+      var busy = false;
+      console.log('listening on port 3000');
+  
+      setInterval(function () {
+          if (!busy && startCam==1) {
+              busy = true;
+              campi.getImageAsStream({
+                  width: 640,
+                  height: 480,
+                  shutter: 200000,
+                  timeout: 1,
+                  nopreview: true
+              }, function (err, stream) {
+                  var message = '';
+  
+                  var base64Stream = stream.pipe(base64.encode());
+  
+                  base64Stream.on('data', function (buffer) {
+                      message += buffer.toString();
+                  });
+  
+                  base64Stream.on('end', function () {
+                      io.sockets.emit('image', message);
+                      busy = false;
+                  });
+              });
+          }
+      }, 100);
+  });
+
+//app.listen(3000);
+successlog.info(`Server started on port 3000`);
